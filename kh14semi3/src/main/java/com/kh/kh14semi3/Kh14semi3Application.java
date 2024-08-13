351f8349a57a9e1bde5b116b5331b7cd859aa055
mincheol11 <f438146@gmail.com>
2024-08-13 17:04:37 +0900

    3조 프로젝트 생성

---
+++ kh14semi3/src/main/java/com/kh/kh14semi3/Kh14semi3Application.java
@@ -0,0 +1,13 @@
+package com.kh.kh14semi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kh14semi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kh14semi3Application.class, args);
+	}
+
+}
---
+++ kh14semi3/src/test/java/com/kh/kh14semi3/Kh14semi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.kh14semi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kh14semi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
