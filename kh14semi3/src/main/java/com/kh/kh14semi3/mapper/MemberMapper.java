532e3b7344f18ac95c97f63a3945c5f2027f098a
calm285 <calm285@gmail.com>
2024-08-22 13:22:03 +0900

    MemberMapper 생성

--- kh14semi3/src/main/java/com/kh/kh14semi3/mapper/MemberMapper.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/mapper/MemberMapper.java
@@ -1,5 +1,33 @@
 package com.kh.kh14semi3.mapper;
 
-public class MemberMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import com.kh.kh14semi3.dto.MemberDto;
+
+@Service
+public class MemberMapper implements RowMapper<MemberDto> {
+
+	@Override
+	public MemberDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		MemberDto memberDto = new MemberDto();
+		memberDto.setMemberId(rs.getString("member_id"));
+		memberDto.setMemberPw(rs.getString("member_pw"));		
+		memberDto.setMemberName(rs.getString("membername"));
+		memberDto.setMemberRank(rs.getString("member_Rank"));
+		memberDto.setMemberEmail(rs.getString("member"));
+		memberDto.setMemberCell(rs.getString("member_cell")); 
+		memberDto.setMemberBirth(rs.getString("member_birth"));
+		memberDto.setMemberPost(rs.getString("member_post"));
+		memberDto.setMemberAddress1(rs.getString("member_address1"));
+		memberDto.setMemberAddress2(rs.getString("member_address2"));
+		memberDto.setMemberJoin(rs.getDate("member_join"));
+		memberDto.setMemberLogin(rs.getDate("member_login"));
+		
+		return memberDto;
+	}
+	
 }
