3c1df604c210cc16796d121d9c3cf8f3ccff4924
mincheol11 <f438146@gmail.com>
2024-08-22 12:49:26 +0900

    boardDto,ScheduleDto

--- kh14semi3/src/main/java/com/kh/kh14semi3/dto/BoardDto.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/dto/BoardDto.java
@@ -1,5 +1,18 @@
 package com.kh.kh14semi3.dto;
 
-public class BoardDto {
+import java.sql.Date;
 
+import lombok.Data;
+
+@Data
+public class BoardDto {
+	private  int boardNo;
+	private  String boardWriter;
+	private  String boardType;
+	private  String boardTitle;
+	private  String boardContent;
+	private  Date boardWtime;
+	private  Date boardUtime;
+	private int boardViews;
+	
 }
--- kh14semi3/src/main/java/com/kh/kh14semi3/dto/ScheduleDto.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/dto/ScheduleDto.java
@@ -1,5 +1,17 @@
 package com.kh.kh14semi3.dto;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class ScheduleDto {
+private  int scheduleNo;
+private  String scheduleWriter;
+private  String scheduleType;
+private  String  scheduleTitle;
+private  String  scheduleContent;
+private  Date scheduleWtime;
+
 
 }
