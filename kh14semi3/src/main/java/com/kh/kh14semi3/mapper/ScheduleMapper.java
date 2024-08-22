fc2469318411561beeb62842a712ad657241d49d
mincheol11 <f438146@gmail.com>
2024-08-22 13:07:42 +0900

    boardMapper,ScheduleMapper

--- kh14semi3/src/main/java/com/kh/kh14semi3/mapper/ScheduleMapper.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/mapper/ScheduleMapper.java
@@ -1,5 +1,26 @@
 package com.kh.kh14semi3.mapper;
 
-public class ScheduleMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import com.kh.kh14semi3.dto.ScheduleDto;
+
+@Service
+public class ScheduleMapper implements RowMapper<ScheduleDto> {
+
+	@Override
+	public ScheduleDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		ScheduleDto scheduleDto = new ScheduleDto();
+		scheduleDto.setScheduleNo(rs.getInt("schedule_no"));
+		scheduleDto.setScheduleWriter(rs.getString("schedule_writer"));
+		scheduleDto.setScheduleType(rs.getString("schedule_type"));
+		scheduleDto.setScheduleTitle(rs.getString("schedule_title"));
+		scheduleDto.setScheduleContent(rs.getString("schedule_content"));
+		scheduleDto.setScheduleWtime(rs.getDate("schedule_wtime"));
+		return scheduleDto;
+	}
 
 }
