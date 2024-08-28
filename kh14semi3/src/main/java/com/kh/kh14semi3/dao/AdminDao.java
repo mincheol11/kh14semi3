9807de162a36a25d40a3875ee3a1a589c3a0e6e1
Park8161 <qkraudgh55@gmail.com>
2024-08-28 12:37:38 +0900

    adminDao

--- kh14semi3/src/main/java/com/kh/kh14semi3/dao/AdminDao.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/dao/AdminDao.java
@@ -1,5 +1,7 @@
 package com.kh.kh14semi3.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,14 +18,30 @@ public class AdminDao {
 	private AdminMapper adminMapper;
 	
 	public void insert(AdminDto adminDto) {
-		String sql = "insert into admin("
-				+ "admin_id"
-				+ ") "
-				+ "values(?)";
+		String sql = "insert into admin(admin_id) values(?)";
 		Object[] data = {adminDto.getAdminId()};
 		jdbcTemplate.update(sql, data);
-		
 	}
+	
+	public AdminDto selectOne(String adminId) {
+		String sql = "select * from admin where admin_id = ?";
+		Object[] data = {adminId};
+		List<AdminDto> list = jdbcTemplate.query(sql, adminMapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
+	public boolean update(AdminDto adminDto) {
+		String sql = "update admin set admin_id = ?";
+		Object[] data = {adminDto.getAdminId()};
+		return jdbcTemplate.update(sql, data) > 0 ;
+	}
+	
+	public boolean delete(String adminId) {
+		String sql = "delete from admin where admin_id = ?";
+		Object[] data = {adminId};
+		return jdbcTemplate.update(sql, data) > 0 ;
+	}
+	
 	
 	
 }
