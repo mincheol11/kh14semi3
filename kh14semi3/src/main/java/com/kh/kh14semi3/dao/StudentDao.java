3ed16ab3ae980e56ba26d486b8415b9bced31465
calm285 <calm285@gmail.com>
2024-08-28 11:04:01 +0900

    하위테이블 만드는중

--- kh14semi3/src/main/java/com/kh/kh14semi3/dao/StudentDao.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/dao/StudentDao.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.kh14semi3.dto.StudentDto;
 import com.kh.kh14semi3.mapper.StudentMapper;
 
 @Repository
@@ -13,6 +14,17 @@ public class StudentDao {
 	private JdbcTemplate jdbcTemplate;
 	@Autowired
 	private StudentMapper studentMapper;
+	
+	public void insert(StudentDto studentDto) {
+		String sql = "insert into student("
+					+ "student_id, department_code, student_level "
+					+ ") "
+					+ "values(?, ?, ?)";
+		Object[] data = {
+				studentDto.getStudentId(), studentDto.getStudentDepartment(), studentDto.getStudentLevel()
+		};
+		jdbcTemplate.update(sql, data);
+	}
 	
 	
 }
