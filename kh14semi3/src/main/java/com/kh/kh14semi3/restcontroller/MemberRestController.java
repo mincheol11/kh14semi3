acf60d58ae621ee4baa83815ccea02ee25a5fe08
calm285 <calm285@gmail.com>
2024-08-30 09:57:17 +0900

    하위 테이블 없으면 아이디는 만들어 지지만 없는 아이디로 처리하여 만들 수 있도록 처리(하는중)

--- kh14semi3/src/main/java/com/kh/kh14semi3/restcontroller/MemberRestController.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/restcontroller/MemberRestController.java
@@ -1,5 +1,6 @@
 package com.kh.kh14semi3.restcontroller;
 
+import org.apache.jasper.tagplugins.jstl.core.Remove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.kh14semi3.dao.AdminDao;
 import com.kh.kh14semi3.dao.MemberDao;
+import com.kh.kh14semi3.dao.ProfessorDao;
+import com.kh.kh14semi3.dao.StudentDao;
+import com.kh.kh14semi3.dto.AdminDto;
 import com.kh.kh14semi3.dto.MemberDto;
+import com.kh.kh14semi3.dto.ProfessorDto;
+import com.kh.kh14semi3.dto.StudentDto;
  
 @CrossOrigin(origins = {"http://127.0.0.1:5500"})
 @RestController
@@ -18,10 +25,27 @@ public class MemberRestController {
 	@Autowired
 	private MemberDao memberDao;
 	
+	@Autowired
+	private StudentDao studentDao;
+	
+	@Autowired
+	private ProfessorDao professorDao;
+	
+	@Autowired
+	private AdminDao adminDao;
+	
 	//아이디 중복 여부 확인
 	@PostMapping("/checkId")
 	public boolean checkId(@RequestParam String memberId) {
 		MemberDto memberDto = memberDao.selectOne(memberId);
+		StudentDto studentDto = studentDao.selectOne(memberId);
+		ProfessorDto professorDto = professorDao.selectOne(memberId);
+		AdminDto adminDto = adminDao.selectOne(memberId);
+		boolean nullCheck = memberDto != null && studentDto == null 
+									&& professorDto == null && adminDto == null;
+		if(nullCheck) {
+			memberDao.delete(memberId);
+		}
 		return memberDto == null;
 	}
 	
