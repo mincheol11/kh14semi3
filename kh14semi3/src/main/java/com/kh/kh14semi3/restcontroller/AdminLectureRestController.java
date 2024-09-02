3de185085f1593d2a17edee307b64c1896b5e88a
Park8161 <qkraudgh55@gmail.com>
2024-09-02 14:07:07 +0900

    학과코드, 교수코드 중복 검사 오류 수정

--- kh14semi3/src/main/java/com/kh/kh14semi3/restcontroller/AdminLectureRestController.java
+++ kh14semi3/src/main/java/com/kh/kh14semi3/restcontroller/AdminLectureRestController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.kh14semi3.dao.AdminLectureDao;
+import com.kh.kh14semi3.dao.DepartmentDao;
+import com.kh.kh14semi3.dao.ProfessorDao;
+import com.kh.kh14semi3.dto.DepartmentDto;
 import com.kh.kh14semi3.dto.LectureDto;
+import com.kh.kh14semi3.dto.ProfessorDto;
 
 @CrossOrigin(origins = {"http://127.0.0.1:5500"})
 @RestController
@@ -16,6 +20,10 @@ import com.kh.kh14semi3.dto.LectureDto;
 public class AdminLectureRestController {
 	@Autowired
 	private AdminLectureDao adminLectureDao;
+	@Autowired
+	private ProfessorDao professorDao;
+	@Autowired
+	private DepartmentDao departmentDao;
 	//코드 중복 검사
 			@PostMapping("/checkLectureCode")
 			public boolean checkLectureCode(@RequestParam String lectureCode) {
@@ -26,15 +34,15 @@ public class AdminLectureRestController {
 			//과목코드 DB에 있는것만 검사
 			@PostMapping("/checkLectureDepartment")
 			public boolean checkLectureDepartment(@RequestParam String lectureDepartment) {
-				LectureDto lectureDto =
-						adminLectureDao.selectOneByLectureDepartment(lectureDepartment);
+				DepartmentDto lectureDto =
+						departmentDao.selectOne(lectureDepartment);
 				return lectureDto!=null; 
 			}
 			//교수코드 DB에 있는것만 검사
 			@PostMapping("/checkLectureProfessor")
 			public boolean checkLectureProfessor(@RequestParam String lectureProfessor) {
-				LectureDto lectureDto =
-						adminLectureDao.selectOneByLectureProfessor(lectureProfessor);
+				ProfessorDto lectureDto =
+						professorDao.selectOne(lectureProfessor);
 				return lectureDto!=null; 
 			}
 			//강의명 중복검사
